881b9720574479268335c4ef0aa94a56fbfadcb2
Svitlana Rybakova <rsv0011@gmail.com>
2023-07-12 12:30:53 +0200

    connect tailwind, and styles for userList

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import './App.css';
+
 import UserList from './components/UserList';
 
 function App() {
   return (
     <>
-      <div className='App'>hello</div>
       <UserList />
     </>
   );
--- src/components/UserList.tsx
+++ src/components/UserList.tsx
@@ -7,17 +7,49 @@ const UserList: React.FC = () => {
   const { users, loading, error } = useTypeSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  if (users) console.log(users);
-
   useEffect(() => {
     dispatch(fetchUsers());
   }, []);
   return (
-    <div>
+    <div className='container mx-auto'>
       {loading && <div>loading</div>}
       {error && <div>{error}</div>}
       {users.length > 0 &&
-        users.map((user) => <div key={user.id}>{user.name}</div>)}
+        users.map((user) => (
+          <ul
+            style={{ borderBottom: '1px solid #80808057' }}
+            role='list'
+            className='cursor-pointer hover:bg-[#d0d0d0ad]'
+            key={user.id}
+          >
+            <li className='flex justify-between gap-x-6 p-5'>
+              <div className='flex gap-x-4'>
+                <img
+                  className='h-12 w-12 flex-none rounded-full bg-gray-50'
+                  src='https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
+                  alt=''
+                />
+                <div className='min-w-0 flex-auto'>
+                  <p className='text-sm font-semibold leading-6 text-gray-900'>
+                    {user.name}
+                  </p>
+                  <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
+                    {user.email}
+                  </p>
+                </div>
+              </div>
+
+              <div className='hidden sm:flex sm:flex-col sm:items-end'>
+                <p className='text-sm leading-6 text-gray-900'>
+                  <em className=''>Company:</em> {user.company.name}
+                </p>
+                <p className='mt-1 text-xs leading-5 text-gray-500'>
+                  {user.website}
+                </p>
+              </div>
+            </li>
+          </ul>
+        ))}
     </div>
   );
 };
