4db7b5e01623ecabd4066a8939766c05028cba41
Svitlana Rybakova <rsv0011@gmail.com>
2023-07-13 16:15:09 +0200

    implement navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,20 @@ import { Route, Routes } from 'react-router-dom';
 
 import UserList from './pages/UserList';
 import UserPage from './pages/UserPage';
+import Todos from './pages/Todos';
+import Navbar from './components/Navbar';
+
 
 function App() {
   return (
     <>
+      <Navbar />
       <Routes>
         <Route path='/users/:id' element={<UserPage />} />
         <Route path='/' element={<UserList />} />
+        <Route path='/todos' element={<Todos />} />
       </Routes>
+      
     </>
   );
 }
--- src/pages/UserList.tsx
+++ src/pages/UserList.tsx
@@ -16,45 +16,50 @@ const UserList: React.FC = () => {
   }, []);
 
   return (
-    <div className='container mx-auto'>
-      {loading && <div>loading</div>}
-      {error && <div>{error}</div>}
-      <ul style={{ borderBottom: '1px solid #80808057' }} role='list'>
-        {users.length > 0 &&
-          users.map((user) => (
-            <li
-              key={user.id}
-              onClick={() => navigate(`/users/${user.id}`)}
-              className='flex justify-between gap-x-6 p-5 cursor-pointer hover:bg-[#d0d0d0ad]'
-            >
-              <div className='flex gap-x-4'>
-                <img
-                  className='h-12 w-12 flex-none rounded-full bg-gray-50'
-                  src={noImage}
-                  alt='image is not available'
-                />
-                <div className='min-w-0 flex-auto'>
-                  <p className='text-sm font-semibold leading-6 text-gray-900'>
-                    {user.name}
+    <>
+      <h2 className='m-4 text-xl  text-center font-bold tracking-tight leading-none text-gray-900 md:text-2xl dark:text-white'>
+        User List
+      </h2>
+      <div className='container mx-auto'>
+        {loading && <div>loading</div>}
+        {error && <div>{error}</div>}
+        <ul style={{ borderBottom: '1px solid #80808057' }} role='list'>
+          {users.length > 0 &&
+            users.map((user) => (
+              <li
+                key={user.id}
+                onClick={() => navigate(`/users/${user.id}`)}
+                className='flex justify-between gap-x-6 p-5 cursor-pointer hover:bg-[#d0d0d0ad]'
+              >
+                <div className='flex gap-x-4'>
+                  <img
+                    className='h-12 w-12 flex-none rounded-full bg-gray-50'
+                    src={noImage}
+                    alt='image is not available'
+                  />
+                  <div className='min-w-0 flex-auto'>
+                    <p className='text-sm font-semibold leading-6 text-gray-900'>
+                      {user.name}
+                    </p>
+                    <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
+                      {user.email}
+                    </p>
+                  </div>
+                </div>
+
+                <div className='hidden sm:flex sm:flex-col sm:items-end'>
+                  <p className='text-sm leading-6 text-gray-900'>
+                    <em className=''>Company:</em> {user.company.name}
                   </p>
-                  <p className='mt-1 truncate text-xs leading-5 text-gray-500'>
-                    {user.email}
+                  <p className='mt-1 text-xs leading-5 text-gray-500'>
+                    {user.website}
                   </p>
                 </div>
-              </div>
-
-              <div className='hidden sm:flex sm:flex-col sm:items-end'>
-                <p className='text-sm leading-6 text-gray-900'>
-                  <em className=''>Company:</em> {user.company.name}
-                </p>
-                <p className='mt-1 text-xs leading-5 text-gray-500'>
-                  {user.website}
-                </p>
-              </div>
-            </li>
-          ))}
-      </ul>
-    </div>
+              </li>
+            ))}
+        </ul>
+      </div>
+    </>
   );
 };
 
