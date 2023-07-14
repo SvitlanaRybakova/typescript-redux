ea6e6010e26cf1374e243f2f49b20091f1387952
Svitlana Rybakova <rsv0011@gmail.com>
2023-07-14 19:44:13 +0200

    imlement the active link

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,22 @@
 import React from 'react';
-import logo from '../assets/logo.png';
 import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+import { v4 as uuidv4 } from 'uuid';
 
+import logo from '../assets/logo.png';
+
+const router = [
+  {
+    id: uuidv4(),
+    to: '/',
+    title: 'Users',
+  },
+  {
+    id: uuidv4(),
+    to: '/todos',
+    title: 'Todos',
+  },
+];
 const Navbar = () => {
   return (
     <nav className='border-gray-200 bg-gray-50 dark:bg-gray-800 dark:border-gray-700'>
@@ -35,23 +50,21 @@ const Navbar = () => {
         </button>
         <div className='hidden w-full md:block md:w-auto' id='navbar-solid-bg'>
           <ul className='flex flex-col font-medium mt-4 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-transparent dark:bg-gray-800 md:dark:bg-transparent dark:border-gray-700'>
-            <li>
-              <NavLink
-                to='/'
-                className='block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500 dark:bg-blue-600 md:dark:bg-transparent'
-                aria-current='page'
-              >
-                Users
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to='/todos'
-                className='block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'
-              >
-                Todos
-              </NavLink>
-            </li>
+            {router.map((pageLink) => (
+              <li key={pageLink.id}>
+                <NavLink
+                  to={pageLink.to}
+                  className={({ isActive }) =>
+                    isActive
+                      ? 'block py-2 pl-3 pr-4 rounded text-white bg-blue-700 md:bg-transparent md:text-blue-700 md:p-0'
+                      : 'block py-2 pl-3 pr-4 rounded text-gray-900 hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0'
+                  }
+                  aria-current='page'
+                >
+                  {pageLink.title}
+                </NavLink>
+              </li>
+            ))}
           </ul>
         </div>
       </div>
